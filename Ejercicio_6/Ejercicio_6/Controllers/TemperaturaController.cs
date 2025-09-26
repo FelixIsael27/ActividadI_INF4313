1f9d1d4443e586bc028861a270f8e59e9fae0b09
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-09-26 18:17:43 -0400

    Ejercicio Finalizado

---
+++ Ejercicio_6/Ejercicio_6/Controllers/TemperaturaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ejercicio_6.Models;
+
+namespace Ejercicio_6.Controllers
+{
+    public class TemperaturaController : Controller
+    {
+        public IActionResult Convertir(double fahrenheit)
+        {
+            var model = new Temperatura
+            {
+                Fahrenheit = fahrenheit,
+                Celsius = (fahrenheit - 32) * 5 / 9
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Ejercicio_6/Ejercicio_6/Models/Temperatura.cs
@@ -0,0 +1,8 @@
+﻿namespace Ejercicio_6.Models
+{
+    public class Temperatura
+    {
+        public double Fahrenheit { get; set; }
+        public double Celsius { get; set; }
+    }
+}
