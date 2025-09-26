5d0180538982b8be7dad22b44a190a085d6bc2df
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-09-26 17:51:38 -0400

    Ejercicio finalizado

---
+++ Ejercicio_4/Ejercicio_4/Controllers/AnagramaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ejercicio_4.Models;
+
+namespace Ejercicio_4.Controllers
+{
+    public class AnagramaController : Controller
+    {
+        public IActionResult Verificar(string texto1, string texto2)
+        {
+            var model = new AnagramaModel
+            {
+                Texto1 = texto1,
+                Texto2 = texto2,
+                Es_o_NoAnagrama = false
+            };
+
+            if (!string.IsNullOrWhiteSpace(texto1) && !string.IsNullOrWhiteSpace(texto2))
+            {
+                var normalizado1 = new string(texto1.ToLower().Replace(" ", "").OrderBy(c => c).ToArray());
+                var normalizado2 = new string(texto2.ToLower().Replace(" ", "").OrderBy(c => c).ToArray());
+
+                model.Es_o_NoAnagrama = normalizado1 == normalizado2;
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Ejercicio_4/Ejercicio_4/Models/AnagramaModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Ejercicio_4.Models
+{
+    public class AnagramaModel
+    {
+        public string Texto1 { get; set; }
+        public string Texto2 { get; set; }
+        public bool Es_o_NoAnagrama { get; set; }
+    }
+}
