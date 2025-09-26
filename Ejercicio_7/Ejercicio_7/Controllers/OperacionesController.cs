9231217f9d4eb73edb9dc882a1ee47fd5d1af8fa
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-09-26 18:38:03 -0400

    Ejercicio finalizado

---
+++ Ejercicio_7/Ejercicio_7/Controllers/OperacionesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ejercicio_7.Models;
+
+namespace Ejercicio_7.Controllers
+{
+    public class OperacionesController : Controller
+    {
+        public IActionResult Sumar(int a, int b)
+        {
+            var operacion = new Operaciones
+            {
+                Numero1 = a,
+                Numero2 = b,
+                Nombre_Operacion = "Suma",
+                Resultado = a + b
+            };
+            return View("Resultado", operacion);
+        }
+
+        public IActionResult Restar(int a, int b)
+        {
+            var operacion = new Operaciones
+            {
+                Numero1 = a,
+                Numero2 = b,
+                Nombre_Operacion = "Resta",
+                Resultado = a - b
+            };
+            return View("Resultado", operacion);
+        }
+
+        public IActionResult Multiplicar(int a, int b)
+        {
+            var operacion = new Operaciones
+            {
+                Numero1 = a,
+                Numero2 = b,
+                Nombre_Operacion = "Multiplicación",
+                Resultado = a * b
+            };
+            return View("Resultado", operacion);
+        }
+
+        public IActionResult Residuo(int a, int b)
+        {
+            var operacion = new Operaciones
+            {
+                Numero1 = a,
+                Numero2 = b,
+                Nombre_Operacion = "Residuo",
+                Resultado = b != 0 ? a % b : double.NaN
+            };
+            return View("Resultado", operacion);
+        }
+    }
+}
---
+++ Ejercicio_7/Ejercicio_7/Models/Operaciones.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio_7.Models
+{
+    public class Operaciones
+    {
+        public int Numero1 { get; set; }
+        public int Numero2 { get; set; }
+        public string Nombre_Operacion { get; set; }
+        public double Resultado { get; set; }
+    }
+}
