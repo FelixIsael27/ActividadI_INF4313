cea90e33624b53a0c74379ae653eca1fabfa6c70
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-09-26 18:10:21 -0400

    Ejercicio Finalizado

---
+++ Ejercicio_5/Ejercicio_5/Controllers/NumeroController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ejercicio_5.Models;
+
+namespace Ejercicio_5.Controllers
+{
+    public class NumeroController : Controller
+    {
+        public IActionResult Tabla(int numero)
+        {
+            var model = new TabladeMultiplicar
+            {
+                Numero = numero
+            };
+
+            for (int i = 1; i <= 10; i++)
+            {
+                model.Resultados.Add($"{numero} x {i} = {numero * i}");
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Ejercicio_5/Ejercicio_5/Models/TabladeMultiplicar.cs
@@ -0,0 +1,8 @@
+﻿namespace Ejercicio_5.Models
+{
+    public class TabladeMultiplicar
+    {
+        public int Numero { get; set; }
+        public List<string> Resultados { get; set; } = new List<string>();
+    }
+}
