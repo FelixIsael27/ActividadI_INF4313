a76e2d48946c16005b55fe3894e3c9529d1cae92
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-09-26 19:17:37 -0400

    Ejercicio finalizado

---
+++ Ejercicio_9/Ejercicio_9/Controllers/FechaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ejercicio_9.Models;
+
+namespace Ejercicio_9.Controllers
+{
+    public class FechaController : Controller
+    {
+        [HttpGet]
+        public IActionResult DiaSemana()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DiaSemana(FechaModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.DiaSemana = model.Fecha.ToString("dddd", new System.Globalization.CultureInfo("es-ES"));
+                return View(model);
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Ejercicio_9/Ejercicio_9/Models/FechaModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ejercicio_9.Models
+{
+    public class FechaModel
+    {
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+
+        public string DiaSemana { get; set; }
+    }
+}
