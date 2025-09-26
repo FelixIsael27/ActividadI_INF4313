72ca0a62a79361a3ceec7954a646a54cfc274439
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-09-26 18:55:33 -0400

    Ejercicio finalizado

---
+++ Ejercicio_8/Ejercicio_8/Controllers/PrimoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ejercicio_8.Models;
+
+namespace Ejercicio_8.Controllers
+{
+    public class PrimoController : Controller
+    {
+        public IActionResult Verificar(int numero)
+        {
+            var model = new PrimoModel
+            {
+                Numero = numero,
+                Es_o_No_Primo = Es_o_No_Primo(numero)
+            };
+
+            return View(model);
+        }
+
+        private bool Es_o_No_Primo(int numero)
+        {
+            if (numero <= 1) return false;
+            if (numero == 2) return true;
+
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Ejercicio_8/Ejercicio_8/Models/PrimoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Ejercicio_8.Models
+{
+    public class PrimoModel
+    {
+        public int Numero { get; set; }
+        public bool Es_o_No_Primo { get; set; }
+    }
+}
