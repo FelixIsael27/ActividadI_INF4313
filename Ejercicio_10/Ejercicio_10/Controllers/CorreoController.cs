685e0850b94ea8bd4ddb6747df7059c2b2a30986
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-09-26 19:34:40 -0400

    Ejercicio finalizado

---
+++ Ejercicio_10/Ejercicio_10/Controllers/CorreoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ejercicio_10.Models;
+
+namespace Ejercicio_10.Controllers
+{
+    public class CorreoController : Controller
+    {
+        public IActionResult Usuario(string email)
+        {
+            var model = new CorreoModel();
+
+            if (string.IsNullOrEmpty(email) || !email.Contains("@"))
+            {
+                model.Mensaje_Error = "Correo inválido";
+            }
+            else
+            {
+                model.Email = email;
+                model.Nombre_Usuario = email.Split('@')[0];
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Ejercicio_10/Ejercicio_10/Models/CorreoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Ejercicio_10.Models
+{
+    public class CorreoModel
+    {
+        public string Email { get; set; }
+        public string Nombre_Usuario { get; set; }
+        public string Mensaje_Error { get; set; }
+    }
+}
